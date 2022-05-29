3d08a67a173ccee098e964b96e9f185291d48c5b
yym10004 <yym10004@naver.com>
2022-05-29 15:07:48 +0900

    Initial commit

---
+++ src/main/java/com/shop/entity/Item.java
@@ -0,0 +1,50 @@
+package com.shop.entity;
+
+import com.shop.constant.ItemSellStatus;
+import com.shop.dto.ItemFormDto;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "item")
+@Getter
+@Setter
+@ToString
+public class Item extends BaseEntity{
+
+    @Id
+    @Column(name = "item_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String itemNm;
+
+    @Column(name = "price", nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private int stockNumber;
+
+    @Lob
+    @Column(nullable = false)
+    private String itemDetail;
+
+    @Enumerated(EnumType.STRING)
+    private ItemSellStatus itemSellstatus;
+
+
+
+    public void updateItem(ItemFormDto itemFormDto){
+        this.itemNm = itemFormDto.getItemNm();
+        this.price = itemFormDto.getPrice();
+        this.stockNumber = itemFormDto.getStockNumber();
+        this.itemDetail = itemFormDto.getItemDetail();
+        this.itemSellstatus = itemFormDto.getItemSellStatus();
+    }
+
+}
